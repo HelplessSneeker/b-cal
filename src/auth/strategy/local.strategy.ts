8b818766add32e7fb8170713c4c6c6e5fc622a8c
bfn <benjamin@noessler.at>
2026-02-02 12:12:58 +0100

    added message to failed login attempt

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -13,7 +13,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<JwtUser> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Login failed');
     }
     return user;
   }
